f67a4c54b403959a2b416f98e50a038b1b1a101f
Aleksandar Dimitrievki <sizowork@gmail.com>
2026-02-27 19:18:58 +0100

    navbar

---
+++ src/app/components/HamburgerMenu.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { navLinks } from "../data";
+
+export default function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button className="ham-menu-btn" onClick={() => setIsOpen(!isOpen)}>
+        <FontAwesomeIcon icon={faBars} className="fa-2x" />
+      </button>
+      {isOpen && (
+        <div className="ham-menu">
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link}>
+                <a href={`#${link.toLowerCase()}`}>{link}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { navLinks } from "../data";
+import "../styles/navbar.css";
+import HamburgerMenu from "./HamburgerMenu";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="nav-container">
+          <h2>Aleksandar Dimitrievski</h2>
+          <ul className="menu">
+            {navLinks.map((link) => (
+              <li key={link}>
+                <a href={`#${link.toLowerCase()}`}>{link}</a>
+              </li>
+            ))}
+          </ul>
+          <a className="connect-link menu" href="">
+            Let's Connect
+          </a>
+          <HamburgerMenu />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
